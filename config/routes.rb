bcaa6aaec71f1a65e324b129d41c4b2bdcb3b4e0
Abby Howell <abby.g.howell@gmail.com>
2013-11-02 17:53:13 -0400

    devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BostonAdaptiveClimbing::Application.routes.draw do
+  root to: "home#index"
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
